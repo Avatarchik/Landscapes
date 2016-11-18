265aae58ba5ea17ff602fcf81828daf74b066ab3
Kaonuri <hon454@naver.com>
2016-11-18 18:11:27 +0900

    Skip->Next

--- FearOfHeight/Assets/02.Scripts/UI/PlayWindow.cs
+++ FearOfHeight/Assets/02.Scripts/UI/PlayWindow.cs
@@ -1,6 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using I2.Loc;
 
 public class PlayWindow : FOHUIWindow
 {
+    public override void ManualUpdate()
+    {
+        base.ManualUpdate();
+        if (game.FohStage.nowStageType == StageType.N_S5)
+        {
+            if (game.FohStage.nowLevelType == LevelType.LV4)
+            {
+                if (game.FohStage.mediaPlayer.Control.GetCurrentTimeMs() >= 15f)
+                {
+                    // game.sounds.Play("FOH_Cityscapes_Main_001 " + LocalizationManager.CurrentLanguage, game.NAVI);
+                }
+            }
+
+            if (game.FohStage.nowLevelType == LevelType.LV5)
+            {
+                if (game.FohStage.mediaPlayer.Control.GetCurrentTimeMs() >= 15f)
+                {
+                    // game.sounds.Play("FOH_Cityscapes_Main_001 " + LocalizationManager.CurrentLanguage, game.NAVI);
+                }
+            }
+        }
+    }
 }
