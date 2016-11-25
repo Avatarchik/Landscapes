ca73703394f68afa3814ae3be2858f392df24d38
Kaonuri <hon454@naver.com>
2016-11-25 19:13:35 +0900

    Record

--- FearOfHeight/Assets/02.Scripts/FirstPersonCamera.cs
+++ FearOfHeight/Assets/02.Scripts/FirstPersonCamera.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FirstPersonCamera : MonoBehaviour {
+public class FirstPersonCamera : MonoBehaviour
+{
 
-    #if UNITY_EDITOR
+#if UNITY_EDITOR
 
     public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
     public RotationAxes axes = RotationAxes.MouseXAndY;
@@ -17,21 +18,24 @@ public class FirstPersonCamera : MonoBehaviour {
 
     void Start()
     {
+        Cursor.visible = false;
+
         Rigidbody m_Rigidbody = GetComponent<Rigidbody>();
 
         if (m_Rigidbody)
             m_Rigidbody.freezeRotation = true;
     }
-    
+
     void Update()
     {
         if (axes == RotationAxes.MouseXAndY)
         {
-            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+            float rotationX = transform.eulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
 
             rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
             rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
-            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+            // transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(new Vector3(-rotationY, rotationX, 0)), 0.2f);
         }
 
         else if (axes == RotationAxes.MouseX)
@@ -47,5 +51,5 @@ public class FirstPersonCamera : MonoBehaviour {
             transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
         }
     }
-    #endif
+#endif
 }
